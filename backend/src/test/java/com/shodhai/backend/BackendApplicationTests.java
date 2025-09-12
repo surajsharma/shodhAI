f3a60402730b4bd39feaf28dd49f5b41e06da9c7
suraj sharma <suraj@evenzero.in>
2025-09-12 19:56:08 +0530

    init

---
+++ backend/src/test/java/com/shodhai/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shodhai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
